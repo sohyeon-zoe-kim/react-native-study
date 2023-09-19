259195860748aaed91348951bccaddda94638b6c
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-09-19 17:01:57 +0900

    feat: 친구 리스트, 내 프로필 컴포넌트 재사용하기

--- part2/kakao-friend-list/App.js
+++ part2/kakao-friend-list/App.js
@@ -1,22 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import {  StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
-import { myProfile } from './src/data'
+import { friendProfiles, myProfile } from './src/data'
 
 import Header from './src/Header.js'
-import MyProfile from './src/MyProfile.js';
+import Profile from './src/Profile.js';
 import Margin from './src/Margin.js'
+import Devision from './src/Devision.js'
+import FriendSection from './src/FriendSection';
+import FriendList from './src/FriendList';
 
 export default function App() {
+  const onPressArrow = () => {
+    console.log('clicked arrow')
+  }
+
   return (
     <SafeAreaProvider>
       <SafeAreaView  style={styles.container} edges={['top', 'right', 'left']}>
         <Header />
         <Margin height={10} />
-        <MyProfile 
+        <Profile 
           uri={myProfile.uri}
           name={myProfile.name}
           introduction={myProfile.introduction}
+        />
+        <Margin height={15} />
+        <Devision />
+        <Margin height={12} />
+        <FriendSection
+          friendProfileLen={friendProfiles.length}
+          onPress={onPressArrow}
+        />
+        <FriendList
+          data={friendProfiles}
         />
       </SafeAreaView>
     </SafeAreaProvider>
@@ -27,5 +44,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingHorizontal: 15
   },
 });
---
+++ part2/kakao-friend-list/src/FriendList.js
@@ -0,0 +1,23 @@
+import { ScrollView, View } from "react-native"
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import Profile from "./Profile"
+import Margin from './Margin'
+
+export default (props) => {
+  const insets = useSafeAreaInsets()
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: insets.bottom }}>
+      {props.data.map((item, index) => (
+        <View key={index}>
+          <Profile
+            uri={item.uri}
+            name={item.name}
+            introduction={item.introduction}
+          />
+          <Margin height={13} />
+        </View>
+      ))}
+    </ScrollView>
+  )
+}
