0981c47b3c4e513c90da562ac8ea0325ed50f1ff
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-09-19 16:47:25 +0900

    feat: MyProfile UI 작업 & Margin 재사용 UI 추가

--- part2/kakao-friend-list/App.js
+++ part2/kakao-friend-list/App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import {  StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { myProfile } from './src/data'
 
 import Header from './src/Header.js'
+import MyProfile from './src/MyProfile.js';
+import Margin from './src/Margin.js'
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView  style={styles.container} edges={['top', 'right', 'left']}>
         <Header />
+        <Margin height={10} />
+        <MyProfile 
+          uri={myProfile.uri}
+          name={myProfile.name}
+          introduction={myProfile.introduction}
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
