b2cd8e836c794b9cd976a9c8a2313d2eed9d9f76
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-09-20 13:47:00 +0900

    feat: 숫자, 연산자, 기타 버튼 UI 개발

--- part2/calculate/App.js
+++ part2/calculate/App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import Calculator from './src/Calculator';
 
 export default function App() {
+  const [input, setInput] = useState(0)
+  const [currentOperator, setCurrentOperator] = useState(null)
+  const [result, setResult] = useState(null)
+  const [tempInput, setTempInput] = useState(null)
+  const [tempOperator, setTempOperator] = useState(null)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container} edges={['top', 'right', 'left']}>
+        <Calculator />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
