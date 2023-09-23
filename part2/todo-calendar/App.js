b682d397e4b40f0298c773c58ee9d6823972b7b0
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-09-23 19:35:28 +0900

    feat: FlatList를 이용해 달력 만들기

---
+++ part2/todo-calendar/App.js
@@ -0,0 +1,92 @@
+import dayjs from "dayjs";
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { SimpleLineIcons } from '@expo/vector-icons'
+import { getCalendarColumns, getDayColor, getDayText } from './src/utils';
+
+const columnSize = 35
+
+const Column = ({
+  text,
+  color,
+  opacity,
+}) => {
+  return (
+    <View style={{
+      width: columnSize,
+      height: columnSize,
+      justifyContent: 'center',
+      alignItems:'center'
+    }}>
+      <Text style={{ color, opacity }}>{text}</Text>
+    </View>
+  )
+}
+
+const ArrowButon = ({ onPress, iconName }) => {
+  return (
+    <TouchableOpacity style={{ paddingHorizontal: 20, paddingVertical: 15 }} onPress={onPress}>
+      <SimpleLineIcons name={iconName} size={15} color='#404040' />
+    </TouchableOpacity>
+  )
+}
+
+export default function App() {
+  const now = dayjs()
+  const columns = getCalendarColumns(now)
+
+  const ListHeaderComponent = () => {
+    const currentDateText = dayjs(now).format('YYYY.MM.DD.')
+    return (
+      <View>
+        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+          <ArrowButon iconName='arrow-left' onPress={() => {}} />
+          <Text style={{ fontSize: 20, color: '#404040' }}>{currentDateText}</Text>
+          <ArrowButon iconName='arrow-right' onPress={() => {}} />
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          {[0, 1, 2, 3, 4, 5, 6].map(day => {
+            const dayText = getDayText(day)
+            const color = getDayColor(day)
+            return (
+              <Column key={day} text={dayText} color={color} opacity={1} />
+            )
+          })}
+        </View>
+      </View>
+    )
+  }
+
+  const renderItem = ({ item: date }) => {
+    const dateText = dayjs(date).get('date')
+    const day = dayjs(date).get('day')
+    const color = getDayColor(day)
+    const isCurrentMonth = dayjs(date).isSame(now, 'month')
+    return (
+      <Column text={dateText} color={color} opacity={isCurrentMonth ? 1 : 0.4} /> 
+    )
+  }
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}> 
+        <FlatList
+          data={columns}
+          keyExtractor={(_, index) => `column-${index}`}
+          numColumns={7}
+          renderItem={renderItem}
+          ListHeaderComponent={ListHeaderComponent}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
