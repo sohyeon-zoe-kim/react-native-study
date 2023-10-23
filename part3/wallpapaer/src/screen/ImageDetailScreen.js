367e2a4fce4431fc1bb8279eddeb041277624a99
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-10-23 16:11:16 +0900

    feat: 이미지 다운로드 버튼 UI & 기능 작성

--- part3/wallpapaer/src/screen/ImageDetailScreen.js
+++ part3/wallpapaer/src/screen/ImageDetailScreen.js
@@ -1,15 +1,56 @@
-import React, { useCallback } from "react"
-import { View, useWindowDimensions } from "react-native"
+import React, { useCallback, useState } from "react"
+import { View, useWindowDimensions, ActivityIndicator } from "react-native"
 import { useNavigation, useRoute } from "@react-navigation/native"
+import * as FileSystem from 'expo-file-system'
+import * as MediaLibrary from 'expo-media-library'
+
 import { Header } from "../components/header/Header"
 import { RemoteImage } from '../components/atoms/RemoteImage'
+import { Button } from "../components/atoms/Button"
+import { Typography } from "../components/atoms/Typography"
+import { Icon } from "../components/atoms/Icon"
 
 export const ImageDetailScreen = (props) => {
   const navigation = useNavigation()
   const route = useRoute()
+  const [downloading, setDownloading] = useState(false)
+
   const onPressBack = useCallback(() => {
     navigation.goBack()
   }, [])
+
+  const onPressDownload = useCallback(async() => {
+    setDownloading(true)
+    const downloadResumable = FileSystem.createDownloadResumable(
+      route.params.url,
+      `${FileSystem.documentDirectory}${new Date().getMilliseconds()}.jpg`
+    )
+
+    try {
+      const { uri } = await downloadResumable.downloadAsync();
+      const permissionResult = await MediaLibrary.getPermissionsAsync(true)
+
+      if (permissionResult.status === 'denied') {
+        setDownloading(false)
+        return
+      }
+
+      if (permissionResult.status === 'undetermined') {
+        const requestResult = await MediaLibrary.requestPermissionsAsync()
+        if (requestResult.status === 'denied') {
+          setDownloading(false)
+          return
+        }
+      }
+
+      const asset = await MediaLibrary.createAssetAsync(uri)
+      MediaLibrary.createAlbumAsync('MyFirstAlbum', asset, false)
+    } catch (e) {
+      console.error(e);
+    }
+    setDownloading(false)
+  }, [])
+
   const { width } = useWindowDimensions() 
 
   return (
@@ -23,6 +64,20 @@ export const ImageDetailScreen = (props) => {
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <RemoteImage url={route.params.url} width={width} height={width * 1.5} />
       </View>
+      <Button onPress={onPressDownload}>
+        <View style={{ paddingBottom: 24, backgroundColor: 'black' }}>
+          {downloading ? (
+            <View style={{ height: 52, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+              <ActivityIndicator />
+            </View>
+          ) : (
+            <View style={{ height: 52, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+              <Typography fontSize={18} color='white'>DOWNLOAD</Typography>
+              <Icon name='download' size={24} color='white' />
+            </View>
+          )}
+        </View>
+      </Button>
     </View>
   )
 }
