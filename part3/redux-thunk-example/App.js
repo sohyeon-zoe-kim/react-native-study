4d2cf9b81d72c1d7b00db96f6884108cdbb66bad
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-10-24 15:37:37 +0900

    feat: redux-thunk-example 셋팅

---
+++ part3/redux-thunk-example/App.js
@@ -0,0 +1,10 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+// import { CountScreen } from './src/screens/CountScreen';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+        {/* <CountScreen /> */}
+    </SafeAreaProvider>
+  );
+}
