88ce4321a00e7f59833f6cf8887c0917cb513df4
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-10-25 21:26:33 +0900

    feat: 로또 번호 생성을 위한 난수 추출 로직 작성

--- part3/lotto-number-get/src/screens/HomeScreen.js
+++ part3/lotto-number-get/src/screens/HomeScreen.js
@@ -1,14 +1,17 @@
-import React, { useCallback } from "react"
+import React, { useCallback, useState } from "react"
 import { View } from "react-native"
 import { Header } from '../components/header/Header'
 import { Spacer } from "../components/atoms/Spacer"
 import { Button } from '../components/atoms/Button'
 import { Typography } from "../components/atoms/Typography"
 import { LottoNumberView } from "../components/LottoNumberView"
+import { getRandomSixNumber } from "../utils/utils"
 
 export const HomeScreen = (props) => {
+  const [numbers, setNumbers] = useState([])
   const onPressGetNumber = useCallback(() => {
-
+    const randomNumber = getRandomSixNumber()
+    setNumbers(randomNumber)
   }, [])
 
   return (
@@ -25,7 +28,9 @@ export const HomeScreen = (props) => {
             borderColor: 'gray',
           }}
         >
-          <LottoNumberView numbers={[1,2,3,4,5,6]} />
+          {numbers.length === 6 && (
+            <LottoNumberView numbers={numbers} />
+          )}
         </View>
         <Spacer space={20} />
         <Button onPress={onPressGetNumber}>
