a1599d81edc0a4945b88e16c4e8434ad1900d137
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-10-25 21:53:17 +0900

    feat: Animated를 활용하여 애니메이션 추가하기

--- part3/lotto-number-get/src/components/LottoNumberView.js
+++ part3/lotto-number-get/src/components/LottoNumberView.js
@@ -1,10 +1,27 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useRandom } from "../hooks/useRandom"
 import { Typography } from "./atoms/Typography"
-import { View } from "react-native"
+import { View, Animated } from "react-native"
 
 export const LottoNumberView = (props) => {
+  const [viewHeight, setViewHeight] = useState(0)
   const { getNumberBackgroundColor } = useRandom()
+
+  const [animatedValue] = useState(new Animated.Value(0))
+  const translateY = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-viewHeight * 0.6, 0]
+  })
+
+  useEffect(() => {
+    animatedValue.setValue(0)
+
+    Animated.timing(animatedValue, {
+      duration: 1000,
+      toValue: 1,
+      useNativeDriver: false,
+    }).start()
+  }, [props.numbers])
 
   return (
     <View
@@ -12,12 +29,16 @@ export const LottoNumberView = (props) => {
         flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        overflow: 'hidden',
+      }}
+      onLayout={({ nativeEvent }) => {
+        setViewHeight(nativeEvent.layout.height)
       }}
     >
       {props.numbers.map((item, index) => {
         return (
-          <View
+          <Animated.View
             key={`lotto-num-${index}`}
             style={{
               backgroundColor: getNumberBackgroundColor(),
@@ -25,11 +46,16 @@ export const LottoNumberView = (props) => {
               height: 40,
               borderRadius: 20,
               alignItems: 'center',
-              justifyContent: 'center'
+              justifyContent: 'center',
+              transform: [
+                {
+                  translateY: translateY
+                }
+              ]
             }}
           >
             <Typography fontSize={20} color='white'>{item.toString()}</Typography>
-          </View>
+          </Animated.View>
         )
       })}
     </View>
