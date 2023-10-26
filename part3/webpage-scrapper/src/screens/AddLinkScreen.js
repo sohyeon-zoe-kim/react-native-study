503affdf5fc9e9abfd415e0957cc5308d9876cab
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-10-26 12:42:54 +0900

    feat: TextInput 컴포넌트 작성 및 링크 추가 화면 구성

--- part3/webpage-scrapper/src/screens/AddLinkScreen.js
+++ part3/webpage-scrapper/src/screens/AddLinkScreen.js
@@ -1,10 +1,18 @@
-import React, { useCallback } from "react"
+import React, { useCallback, useState } from "react"
 import { useNavigation } from '@react-navigation/native'
 import { View } from "react-native"
 import { Header } from "../components/header/Header"
+import { SingleLineInput } from "../components/atoms/SingleLineInput"
+import { Button } from "../components/atoms/Button"
+import { Typography } from "../components/atoms/Typography"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { Spacer } from "../components/atoms/Spacer"
 
 export const AddLinkScreen = () => {
   const navigation = useNavigation()
+  const safeAreaInset = useSafeAreaInsets()
+  const [url, setUrl] = useState('')
+
   const onPressClose = useCallback(() => {
     navigation.goBack()
   }, [])
@@ -16,6 +24,21 @@ export const AddLinkScreen = () => {
         </Header.Group>
         <Header.Icon iconName='close' onPress={onPressClose} />
       </Header>      
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', paddingHorizontal: 24 }}>
+        <SingleLineInput
+          value={url}
+          onChangeText={setUrl}
+          placeholder='https://example.com'
+        />
+      </View>
+      <Button>
+        <View style={{ backgroundColor: url === '' ? 'gray' : 'black' }}>
+          <View style={{ height: 52, alignItems: 'center', justifyContent: 'center'}}>
+            <Typography color='white' fontSize={18}>저장하기</Typography>
+          </View>
+          <Spacer space={safeAreaInset.bottom} />
+        </View>
+      </Button>
     </View>
   )
 }
