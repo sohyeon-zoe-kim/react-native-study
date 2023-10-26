8ca83a829d8ec7802621da79f50ed9f4f5fb5bd4
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-10-26 12:25:40 +0900

    feat: part3 - 웹페이지 스크랩 앱 만들기 setting

---
+++ part3/webpage-scrapper/App.js
@@ -0,0 +1,11 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
