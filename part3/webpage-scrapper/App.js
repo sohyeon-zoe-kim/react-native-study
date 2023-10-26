cb9fd1ed7cc7e68975ea09c066c86da171ad0af8
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-10-26 13:19:49 +0900

    feat: recoil, async-storage 추가 / atom 작성

--- part3/webpage-scrapper/App.js
+++ part3/webpage-scrapper/App.js
@@ -1,13 +1,16 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { NavigationContainer } from '@react-navigation/native'
 import { RootNavigation } from './src/navigations/RootNavigation';
+import { RecoilRoot } from 'recoil';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <RootNavigation />
-      </NavigationContainer>
+      <RecoilRoot>
+        <NavigationContainer>
+          <RootNavigation />
+        </NavigationContainer>
+      </RecoilRoot>
     </SafeAreaProvider>
   );
 }
