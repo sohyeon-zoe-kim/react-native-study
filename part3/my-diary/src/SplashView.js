85fc6d46935e35a5cccbfbb3499a012518861238
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-10-29 13:38:45 +0900

    feat: Splash 화면 추가

---
+++ part3/my-diary/src/SplashView.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react"
+import { View } from "react-native"
+import { Typography } from "./components/atoms/Typography"
+
+export const SplashView = (props) => {
+  useEffect(() => {
+    setTimeout(() => {
+      props.onFinishLoad()
+    }, 2000)
+  }, [])
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Typography fontSize={26}>
+        SPLASH
+      </Typography>
+    </View>
+  )
+}
