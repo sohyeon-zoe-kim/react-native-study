8cb9d436ff4419ae82a4062edaf3b8f1f7e8b19b
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-10-30 20:33:23 +0900

    feat: splashView에서 비밀번호가 있는 유저의 경우 입력해야 화면이 전환되도록 설정

--- part3/my-diary/src/SplashView.js
+++ part3/my-diary/src/SplashView.js
@@ -3,14 +3,17 @@ import { View } from "react-native"
 import { GoogleSignin, GoogleSigninButton} from '@react-native-google-signin/google-signin'
 import auth from '@react-native-firebase/auth'
 import database from '@react-native-firebase/database'
-import { useSetRecoilState } from "recoil"
+import { useRecoilState, useSetRecoilState } from "recoil"
 import { stateUserInfo } from "./states/stateUserInfo"
 import { useGetDiaryList } from "./hooks/useGetDiaryList"
+import { PasswordInputBox } from "./components/PasswordInputBox"
 
 export const SplashView = (props) => {
   const [showLoginButton, setShowLoginButton] = useState(false)
-  const setUserInfo = useSetRecoilState(stateUserInfo)
+  const [inputPassword, setInputPassword] = useState('')
+  const [userInfo, setUserInfo] = useRecoilState(stateUserInfo)
   const runGetDiaryList = useGetDiaryList()
+  const [showPasswordInput, setShowPasswordInput] = useState(false)
 
   const signinUserIdentify = useCallback(async (idToken) => {
     const googleCredentials = auth.GoogleAuthProvider.credential(idToken)
@@ -31,16 +34,21 @@ export const SplashView = (props) => {
         lastLoginAt: now
       })
     } else {
-      await database().ref(userDBRefKey).update({
-        lastLoginAt: now,
-      })
+      
     }
 
     const userInfo = await database().ref(userDBRefKey).once('value').then((snapshot) => snapshot.val())
-
     setUserInfo(userInfo)
-
     await runGetDiaryList(userInfo)
+
+    if (userInfo.password !== '') {
+      setShowPasswordInput(true)
+      return
+    }
+
+    await database().ref(userDBRefKey).update({
+      lastLoginAt: now,
+    })
 
     props.onFinishLoad()
   }, [])
@@ -67,6 +75,19 @@ export const SplashView = (props) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       {showLoginButton && <GoogleSigninButton onPress={onPressGoogleLogin} />}
+      {showPasswordInput && <PasswordInputBox value={inputPassword} onChangeText={async(text) => {
+        setInputPassword(text)
+        if (text.length === 4) {
+          if (userInfo.password === text) {
+            const now = new Date().toISOString()
+            const userDB = `/users/${userInfo.uid}/`
+            await database().ref (userDB).update({
+              lastLoginAt: now,
+            })
+            props.onFinishLoad()
+          }
+        }
+      }} />}
     </View>
   )
 }
