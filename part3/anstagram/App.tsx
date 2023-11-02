aa022aa144ad89d7ad851e5d99f7f2eac2671786
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-11-02 13:21:06 +0900

    feat: part3 인스타그램 클론코딩 프로젝트 셋팅
    * 프로젝트 생성, eslint/typescript 셋팅

---
+++ part3/anstagram/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+
+    </SafeAreaProvider>
+  );
+}
