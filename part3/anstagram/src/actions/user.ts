f56d08b403ded52dc6cec93c2988178ccfd35933
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-11-02 15:30:28 +0900

    feat: user의 FeedList를 가져오는 action 작성

--- part3/anstagram/src/actions/user.ts
+++ part3/anstagram/src/actions/user.ts
@@ -1,8 +1,12 @@
 import { ThunkAction } from "redux-thunk"
 import { sleep } from "../utils/sleep"
 import { TypeRootReducer } from "../store"
+import { FeedInfo } from "../types/FeedInfo"
 
 export const SET_USER_INFO = 'SET_USER_INFO' as const
+export const GET_MY_FEED_REQUEST = 'GET_MY_FEED_REQUEST' as const
+export const GET_MY_FEED_SUCCESS = 'GET_MY_FEED_SUCCESS' as const
+export const GET_MY_FEED_FAILURE = 'GET_MY_FEED_FAILURE' as const
 
 export const setUserInfo = (userId: string) => {
   return {
@@ -11,11 +15,73 @@ export const setUserInfo = (userId: string) => {
   }
 }
 
+export const getMyFeedRequest = () => {
+  return {
+    type: GET_MY_FEED_REQUEST,
+  }
+}
+
+export const getMyFeedSuccess = (list: FeedInfo[]) => {
+  return {
+    type: GET_MY_FEED_SUCCESS,
+    list
+  }
+}
+
+export const getMyFeedFailure = () => {
+  return {
+    type: GET_MY_FEED_FAILURE,
+  }
+}
+
 export const signing = (): TypeUserInfoThunkAction => async (dispatch) => {
-  await sleep(1000)
+  await sleep(500)
   dispatch(setUserInfo('TEST'))
+}
+
+export const getMyFeedList = (): TypeUserInfoThunkAction => async (dispatch) => {
+  dispatch(getMyFeedRequest())
+  await sleep(500)
+  dispatch(getMyFeedSuccess([
+    {
+      id: 'ID_01',
+      content: 'CONTENT_01',
+      writer: {
+        name: 'WRITER_NAME_01',
+        uid: 'WRITER_UID_01',
+      },
+      imageUrl: 'IMAGE_URL_01',
+      likeHistory: ['UID_01', 'UID_02', 'UID_03'],
+      createdAt: new Date().getTime()
+    },
+    {
+      id: 'ID_02',
+      content: 'CONTENT_02',
+      writer: {
+        name: 'WRITER_NAME_02',
+        uid: 'WRITER_UID_02',
+      },
+      imageUrl: 'IMAGE_URL_02',
+      likeHistory: ['UID_03'],
+      createdAt: new Date().getTime()
+    },
+    {
+      id: 'ID_03',
+      content: 'CONTENT_03',
+      writer: {
+        name: 'WRITER_NAME_03',
+        uid: 'WRITER_UID_03',
+      },
+      imageUrl: 'IMAGE_URL_03',
+      likeHistory: ['UID_01', 'UID_02'],
+      createdAt: new Date().getTime()
+    },
+  ]))
 }
 
 export type TypeUserInfoThunkAction = ThunkAction<void, TypeRootReducer, undefined, TypeUserInfoActions>
 export type TypeUserInfoActions =
   | ReturnType<typeof setUserInfo>
+  | ReturnType<typeof getMyFeedRequest>
+  | ReturnType<typeof getMyFeedSuccess>
+  | ReturnType<typeof getMyFeedFailure>
