4f9ebfc39fc930ef25127da169f8a4a788a9c68d
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-11-02 13:44:00 +0900

    feat: FeedListItem 컴포넌트 UI 작성

--- part3/anstagram/App.tsx
+++ part3/anstagram/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { FeedListItem } from './src/components/FeedListItem';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <FeedListItem
+          image={"https://cdn.imweb.me/upload/S20200106a105fd03f4b57/ddf34457eaa8b.jpg"}
+          likeCount={10}
+          writer='fstvl.life'
+          comment='This is test!!!'
+          isLiked={false}
+          onPressFeed={() => {
+            console.log('onPress!')
+          }}
+        />
+      </View>
     </SafeAreaProvider>
   );
 }
