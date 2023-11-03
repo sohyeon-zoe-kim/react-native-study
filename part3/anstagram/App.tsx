85df26b58afc0105b8114d2aba32168c1d5096d7
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-11-03 16:01:29 +0900

    feat: DoubleTabButton 컴포넌트 작성, feed 더블탭시 favorite feed 동작하도록 수정

--- part3/anstagram/App.tsx
+++ part3/anstagram/App.tsx
@@ -4,6 +4,7 @@ import { RootApp } from './src/RootApp';
 import { Provider } from 'react-redux';
 import { store } from './src/store';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 GoogleSignin.configure()
 
@@ -11,7 +12,9 @@ export default function App() {
   return (
     <SafeAreaProvider>
       <Provider store={store}>
-        <RootApp />
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <RootApp />
+        </GestureHandlerRootView>
       </Provider>
     </SafeAreaProvider>
   );
--- part3/anstagram/src/components/FeedListItem.tsx
+++ part3/anstagram/src/components/FeedListItem.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { View, useWindowDimensions } from "react-native";
 import { Button } from "./atoms/Button";
 import { RemoteImage } from "./atoms/RemoteImage";
 import { Icon } from "./atoms/Icon";
 import { Typography } from "./atoms/Typography";
 import { Spacer } from "./atoms/Spacer";
+import { DoubleTabButton } from "./atoms/DoubleTabButton";
 
 export const FeedListItem: React.FC<{
   image: string
@@ -16,29 +17,48 @@ export const FeedListItem: React.FC<{
   onPressFavorite: () => void
 }> = (props) => {
   const { width } = useWindowDimensions()
+  const onPressDouobleTap = useCallback(() => {
+    console.log('onPRessDoubleTap')
+    props.onPressFavorite()
+  }, [])
+
   return (
-    <Button onPress={props.onPressFeed}>
-      <View>
-        <RemoteImage url={props.image} width={width} height={width} />
-        <Button onPress={props.onPressFavorite}>
-          <View style={{ paddingHorizontal: 12, paddingVertical: 6 }}>
-            <Icon
-              name={props.isLiked ? "heart" : "heart-outline"}
-              size={20}
-              color={props.isLiked ? "red" : "black"}
-            />
-          </View>
-        </Button>
-        <View style={{ paddingHorizontal: 12 }}>
-          <Typography fontSize={16}>{`좋아요 ${props.likeCount}개`}</Typography>
-          <Spacer space={4} />
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Typography fontSize={16}>{props.writer}</Typography>
-            <Spacer space={8} horizontal />
-            <Typography fontSize={16}>{props.comment}</Typography>
+    <View>
+      <DoubleTabButton onPressDoubleTab={onPressDouobleTap}>
+        <View style={{ width: width, height: width }}>
+          <RemoteImage url={props.image} width={width} height={width} />
+          <View style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+          >
+            <Icon name="heart" size={64} color="red" />
           </View>
         </View>
+      </DoubleTabButton>
+      <Button onPress={props.onPressFavorite}>
+        <View style={{ paddingHorizontal: 12, paddingVertical: 6 }}>
+          <Icon
+            name={props.isLiked ? "heart" : "heart-outline"}
+            size={20}
+            color={props.isLiked ? "red" : "black"}
+          />
+        </View>
+      </Button>
+      <View style={{ paddingHorizontal: 12 }}>
+        <Typography fontSize={16}>{`좋아요 ${props.likeCount}개`}</Typography>
+        <Spacer space={4} />
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Typography fontSize={16}>{props.writer}</Typography>
+          <Spacer space={8} horizontal />
+          <Typography fontSize={16}>{props.comment}</Typography>
+        </View>
       </View>
-    </Button>
+    </View>
   )
 }
