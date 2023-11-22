62501752641d3de4c917fb734e0ad9b50f2b5e75
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-11-22 16:07:31 +0900

    feat: bareworkflow 로 변환

--- part3/anstagram/App.tsx
+++ part3/anstagram/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { RootApp } from './src/RootApp';
-import { Provider } from 'react-redux';
-import { store } from './src/store';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { RootApp } from "./src/RootApp";
+import { Provider } from "react-redux";
+import { store } from "./src/store";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-GoogleSignin.configure()
+GoogleSignin.configure();
 
 export default function App() {
   return (
--- part3/anstagram/src/navigations/BottomTabNavigation.tsx
+++ part3/anstagram/src/navigations/BottomTabNavigation.tsx
@@ -1,27 +1,27 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import React from "react"
-import { HomeScreen } from "../screens/HomeScreen"
-import { MyPageScreen } from "../screens/MyPageScreen"
-import { iconName } from "../components/atoms/Icon"
-import { TabIcon } from "../components/molcules/TabIcon"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { HomeScreen } from "../screens/HomeScreen";
+import { MyPageScreen } from "../screens/MyPageScreen";
+import { iconName } from "../components/atoms/Icon";
+import { TabIcon } from "../components/molcules/TabIcon";
 
 export type BottomTabParamList = {
-  Home: undefined,
-  MyPage: undefined
-}
-const BottomTab = createBottomTabNavigator<BottomTabParamList>()
+  Home: undefined;
+  MyPage: undefined;
+};
+const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 export const BottomTabNaviation = () => {
   return (
     <BottomTab.Navigator
       screenOptions={({ route }) => {
         const getIconName = (): iconName => {
-          if (route.name === 'MyPage') {
-            return 'person'
+          if (route.name === "MyPage") {
+            return "person";
           }
-          return 'home'
-        }
+          return "home";
+        };
 
-        const routeIconName = getIconName()
+        const routeIconName = getIconName();
 
         return {
           headerShown: false,
@@ -32,13 +32,13 @@ export const BottomTabNaviation = () => {
                 iconName={routeIconName}
                 iconColor={color}
               />
-            )
-          }
-        }
+            );
+          },
+        };
       }}
     >
-      <BottomTab.Screen name='Home' component={HomeScreen} />
-      <BottomTab.Screen name='MyPage' component={MyPageScreen} />
-    </BottomTab.Navigator >
-  )
-}
+      <BottomTab.Screen name="Home" component={HomeScreen} />
+      <BottomTab.Screen name="MyPage" component={MyPageScreen} />
+    </BottomTab.Navigator>
+  );
+};
